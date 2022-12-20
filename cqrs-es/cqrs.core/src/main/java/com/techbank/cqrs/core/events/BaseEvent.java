3a2cf467bb226820d994bb49ec32f05c8418c896
Boonkuae Boo <boonkuae.boo@outlook.co.th>
2022-12-21 01:31:33 +0700

    4# Fix superbuilder

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
@@ -3,13 +3,17 @@ package com.techbank.cqrs.core.events;
 import com.techbank.cqrs.core.commands.BaseCommand;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder
 public abstract class BaseEvent extends BaseCommand {
     private int version;
+
+    public BaseEvent(String id) {
+        super(id);
+    }
 }
--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/messages/Message.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/messages/Message.java
@@ -3,6 +3,7 @@ package com.techbank.cqrs.core.messages;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
